55efce1297ebc6fd57041499285de651ff9f036e
dellldcdcdcd <asliddinsalohiddinov115@gmail.com>
2023-05-05 15:06:18 +0500

    calculation

---
+++ app.js
@@ -0,0 +1,153 @@
+function fun1(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun2(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value =+" "
+}
+function fun3(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun4(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="("
+}
+function fun5(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=")"
+}
+function fun6(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="%"
+}
+function fun7(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value  =" "
+}
+function fun8(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun9(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun10(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun11(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="7"
+}
+function fun12(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="8"
+}
+function fun13(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="9"
+}
+function fun14(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="/"
+}
+function fun15(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun16(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun17(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun18(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun19(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="4"
+}
+function fun20(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="5"
+}
+function fun21(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="6"
+}
+function fun22(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="*"
+}
+function fun23(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun24(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun25(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun26(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="1"
+}
+function fun27(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="2"
+}
+function fun28(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="3"
+}
+function fun29(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="-"
+}
+function fun30(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun31(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun32(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +=""
+}
+function fun33(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="0"
+}
+function fun34(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="."
+}
+function fun35(){
+var inp1 = document.getElementById("inp1");
+var typed =inp1.value;
+console.log(eval(typed));
+var result = eval(typed);
+inp1.value = result;
+}
+function fun36(){
+    var inp1 = document.getElementById("inp1");
+    inp1.value +="+"
+}
+function sun1(){
+    var form1 = document.getElementsByClassName("form1");
+
+}
+
+
